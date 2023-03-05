fixes: changed iou computation in rpn loss to be based on anchors like in the paper

model_zoo/oriented_rcnn/losses.py
@@ -6,15 +6,15 @@ from .utils import offsets_to_proposal, midpoint_offset_representation_to_coords
 
 def gt_coords_to_offsets(boxes: torch.Tensor, anchor_boxes: torch.Tensor):
     # input: b, n, 4, 2
-    x_min = torch.min(boxes[:, :, :, 0], dim=2)[0]
-    x_max = torch.max(boxes[:, :, :, 0], dim=2)[0]
-    y_min = torch.min(boxes[:, :, :, 1], dim=2)[0]
-    y_max = torch.max(boxes[:, :, :, 1], dim=2)[0]
+    x_min = torch.min(boxes[:, :, 0], dim=2)[0]
+    x_max = torch.max(boxes[:, :, 0], dim=2)[0]
+    y_min = torch.min(boxes[:, :, 1], dim=2)[0]
+    y_max = torch.max(boxes[:, :, 1], dim=2)[0]
     # anchor: (ax, ay , aw, ah)
-    x_a = anchor_boxes[:, :, 0, :, :]
-    y_a = anchor_boxes[:, :, 1, :, :]
-    w_a = anchor_boxes[:, :, 2, :, :]
-    h_a = anchor_boxes[:, :, 3, :, :]
+    x_a = anchor_boxes[:, 0]
+    y_a = anchor_boxes[:, 1]
+    w_a = anchor_boxes[:, 2]
+    h_a = anchor_boxes[:, 3]
 
     # outer box (HBB) midpoint and width, height
     x_g = x_min + (x_max - x_min) / 2
@@ -35,16 +35,27 @@ def gt_coords_to_offsets(boxes: torch.Tensor, anchor_boxes: torch.Tensor):
 def rpn_loss(anchors: torch.Tensor, anchor_offsets: torch.Tensor, objectness_score: torch.Tensor, ground_truth_boxes: torch.Tensor, cls_sigmoid: bool = True):
     b, _, h, w = anchor_offsets.shape
     stacked_anchor_offsets = anchor_offsets.reshape(b, -1, 6, h, w).permute((0, 1, 3, 4, 2)).flatten(1, 3)
-    proposals = offsets_to_proposal(anchors, anchor_offsets.reshape(b, -1, 6, h, w))
-    proposals = midpoint_offset_representation_to_coords(proposals)
+    # proposals = offsets_to_proposal(anchors, anchor_offsets.reshape(b, -1, 6, h, w))
+    # proposals = midpoint_offset_representation_to_coords(proposals)
     stacked_objectness_scores = objectness_score.flatten(1)
-    iou = rotated_iou(proposals, ground_truth_boxes)
+    a_x = anchors[:, :, 0, :, :]
+    a_y = anchors[:, :, 1, :, :]
+    a_w = anchors[:, :, 2, :, :]
+    a_h = anchors[:, :, 3, :, :]
+    coords_anchors = torch.stack([
+        torch.stack((a_x, a_y), dim=2),
+        torch.stack((a_x + a_w, a_y), dim=2),
+        torch.stack((a_x + a_w, a_y + a_h), dim=2),
+        torch.stack((a_x, a_y + a_h), dim=2)
+    ], dim=2)
+    iou = rotated_iou(coords_anchors, ground_truth_boxes)
     
-    # every proposal without iou < 0.3 is considered a FP regarding objectness_score
+    # why use anchors instead of the proposals directly?
+    # every anchor without iou < 0.3 is considered a FP regarding objectness_score
     fp_mask = (torch.max(iou, dim=2).values < 0.3).nonzero(as_tuple=True)
-    # every proposal with iou > 0.7 is considered a TP regarding objectness_score
+    # every anchor with iou > 0.7 is considered a TP regarding objectness_score
     tp_mask = (torch.max(iou, dim=2).values > 0.7).nonzero(as_tuple=True)
-    # every proposal with iou > 0.3 and the highest overlap for a ground truth bb is also considered TP
+    # every anchor with iou > 0.3 and the highest overlap for a ground truth bb is also considered TP
     max_for_each_gt = torch.max(iou, dim=1)
     cond_idx = ((max_for_each_gt[0] > 0.3) & (max_for_each_gt[0] < 0.7)).nonzero(as_tuple=True)
     additional_tp = (cond_idx[0], max_for_each_gt[1][cond_idx])
@@ -56,16 +67,14 @@ def rpn_loss(anchors: torch.Tensor, anchor_offsets: torch.Tensor, objectness_sco
     cls_loss = 1/b * (negative_cls + positive_cls)
 
     # for regression loss we only consider the true positives
-    gt_targets = gt_coords_to_offsets(ground_truth_boxes, anchors)
-
-    F.smooth_l1_loss(stacked_anchor_offsets)
-
-    test = iou[max_for_each_gt]
-    fp = (iou < 0.3).nonzero(as_tuple=True)
-    tp = (iou > 0.7)
-
-    return iou
-    # batch_size = anchors.shape[0]
-    # loss = 1/batch_size * () + 1/batch_size * ()
-
+    # todo: replace loop with broadcast if possible
+    regr_loss = 0
+    anchors = anchors.permute((0,1,3,4,2)).flatten(1,3)
+    for i in range(b):
+        pos_anchors = torch.cat((anchors[i, tp_mask[i]], anchors[i, additional_tp[i]]))
+        gt_targets = gt_coords_to_offsets(ground_truth_boxes[i], pos_anchors)
+        pred = torch.cat((stacked_anchor_offsets[i, tp_mask[i]], stacked_anchor_offsets[i, additional_tp[i]]))
+        regr_loss = regr_loss + F.smooth_l1_loss(pred, gt_targets)
     
+    regr_loss = 1/b * regr_loss
+    return cls_loss + regr_loss

model_zoo/oriented_rcnn/test/run.py
@@ -7,7 +7,6 @@ import torchvision
 from ..model import OrientedRCNN
 from ..anchor_generation import AnchorGenerator
 from ..losses import rpn_loss
-from ..utils import rotated_iou, fake_2d_to_3d
 import numpy as np
 
 # DETECTRON 2!
@@ -26,6 +25,18 @@ if __name__ == "__main__":
 
     x = torch.tensor(image).permute((2, 0, 1)).unsqueeze(0)[:, :3, :, :] / 255
     x = x.repeat((2, 1, 1, 1))
+
+    for b in ground_truth["boxes"]:
+        x = torch.min(b[:, 0]).to(torch.int).cpu().item() // 2
+        y = torch.min(b[:, 1]).to(torch.int).cpu().item() // 2
+        xmax = torch.max(b[:, 0]).to(torch.int).cpu().item() // 2
+        ymax = torch.max(b[:, 1]).to(torch.int).cpu().item() // 2
+        tl = [x,y] 
+        br = [xmax,ymax] 
+        image = cv2.resize(image, (512, 512))
+        image = cv2.rectangle(image, tl, br, (0, 255, 0), 2)
+
+    cv2.imwrite("test.png", image)
 
     # boxes = []
     # labels = []
@@ -83,6 +94,6 @@ if __name__ == "__main__":
     loss = 0
     for k, v in out["rpn_out"].items():
         #sanity = rpn_loss(anchors[k], ground_truth["boxes"], ground_truth["labels"], ground_truth["boxes"]) 
-        rpn_loss(anchors[k], v["anchor_offsets"], v["objectness_scores"], gt)
+        loss = rpn_loss(anchors[k], v["anchor_offsets"], v["objectness_scores"], gt)
 
     a = 5

model_zoo/oriented_rcnn/utils.py
@@ -3,27 +3,25 @@ from pytorch3d.ops import box3d_overlap
 
 import numpy as np
 
-
-
 def offsets_to_proposal(anchor: torch.Tensor, offset: torch.Tensor):
     # offset: (δx, δy , δw, δh, δα, δβ)
     # anchor: (ax, ay , aw, ah)
     # w = aw · e^{δw}
     w = anchor[:, :, 2, :, :] * torch.exp(offset[:, :, 2, :, :])
-    # deviation from original paper: preventing crooked rectangles
+    # deviation from original paper: preventing rectangles w < 1
     w = torch.maximum(w, torch.ones_like(w))
     # h = ah · eδh
     h = anchor[:, :, 3, :, :] * torch.exp(offset[:, :, 3, :, :])
-    # deviation from original paper: preventing crooked rectangles
+    # deviation from original paper: preventing rectangles w < 1
     h = torch.maximum(h, torch.ones_like(h))
     # ∆α = δα · w 
     delta_alpha = offset[:, :, 4, :, :] * w
-    # deviation from original paper: preventing crooked rectangles
+    # deviation from original paper: restricting alpha to outer rect
     delta_alpha = torch.maximum(delta_alpha, w/2)
     delta_alpha = torch.minimum(delta_alpha, -w/2)
     # ∆β = δβ · h 
     delta_beta = offset[:, :, 5, :, :] * h
-    # deviation from original paper: preventing crooked rectangles
+    # deviation from original paper: restricting beta to outer rect
     delta_beta = torch.maximum(delta_beta, h/2)
     delta_beta = torch.minimum(delta_beta, -h/2)
     # x = δx · aw + ax
@@ -163,7 +161,6 @@ def midpoint_offset_representation_to_coords(repr: torch.Tensor):
 
     return result
 
-    
 
 def fake_2d_to_3d(boxes: torch.Tensor):
     '''
@@ -186,6 +183,7 @@ def fake_2d_to_3d(boxes: torch.Tensor):
     one_depth = torch.concat((torch.zeros(dim), torch.ones((dim))), dim=2).to(boxes.device)
     result = torch.concat((result, one_depth.unsqueeze(3)), dim=3)
     return result
+
 
 def rotated_iou(boxes1: torch.tensor, boxes2: torch.tensor):
     # using 3d for 2d for speed and convenience https://pytorch3d.org/docs/iou3d