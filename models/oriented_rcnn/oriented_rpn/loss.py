ce88889f98f901acfbed55cdcd50f53bba3672bb
Simon Owerien <simon.owerien@live.de>
2023-05-22 18:06:23 +0200

    changes

--- models/oriented_rcnn/oriented_rpn/loss.py
+++ models/oriented_rcnn/oriented_rpn/loss.py
@@ -1,8 +1,7 @@
 import torch
 from torchvision.ops import box_iou
 
-def rpn_anchor_iou(anchors: torch.Tensor, ground_truth: torch.Tensor):
-    assert len(ground_truth.shape) == 4, "expected ground truth shape b, n, 4, 2"
+def flatten_anchors_hbb(anchors: torch.Tensor):
     # anchors are (x, y, w, h) with x,y being the center of the anchor
     # we need to convert to vertices for torchvision box_iou
     b, n, h, w = anchors.shape
@@ -17,7 +16,10 @@ def rpn_anchor_iou(anchors: torch.Tensor, ground_truth: torch.Tensor):
     
     r_anchor_boxes = torch.stack((anchor_x1, anchor_y1, anchor_x2, anchor_y2), dim=2)
     anchor_boxes = r_anchor_boxes.permute((0, 1, 3, 4, 2)).flatten(start_dim=0, end_dim=3)
-    print(anchor_boxes[0])
+    return anchor_boxes
+
+def flatten_ground_truth_hbb(ground_truth: torch.Tensor):
+    assert len(ground_truth.shape) == 4, "expected ground truth shape b, n, 4, 2"
 
     # ground truth shape: b, n, 4, 2
     gt_x1 = torch.min(ground_truth[:, :, :, 0], dim=2)[0]
@@ -27,4 +29,12 @@ def rpn_anchor_iou(anchors: torch.Tensor, ground_truth: torch.Tensor):
     
     r_gt_boxes = torch.stack((gt_x1, gt_y1, gt_x2, gt_y2), dim=2)
     gt_boxes = r_gt_boxes.flatten(start_dim=0, end_dim=1)
+    return gt_boxes
+
+def rpn_anchor_iou(anchors: torch.Tensor, ground_truth: torch.Tensor):
+    anchor_boxes = flatten_anchors_hbb(anchors)
+    gt_boxes = flatten_ground_truth_hbb(ground_truth)
     return box_iou(anchor_boxes, gt_boxes)
+
+def rpn_loss(regression: torch.Tensor, objectness: torch.Tensor, anchors: torch.Tensor, ground_truth: torch.Tensor):
+    pass
--- models/oriented_rcnn/tests/rpn_loss.py
+++ models/oriented_rcnn/tests/rpn_loss.py
@@ -12,6 +12,7 @@ class TestRPNLoss(unittest.TestCase):
         anchors = torch.tensor([10, 10, 10, 5]).view((1, -1, 1, 1)).float().repeat((1, 4, 1, 1))
         ground_truth = torch.tensor([[5, 5], [15, 5], [15, 15], [5, 15]]).unsqueeze(0).unsqueeze(0).float()
         iou = loss.rpn_anchor_iou(anchors, ground_truth)
+        print(iou.shape)
         self.assertTrue(torch.allclose(iou[0][0], torch.tensor([0.5])))
 
 if __name__ == "__main__":
